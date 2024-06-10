a9844aa8503eb6ee0c8680049f732e60003a04f0
CoraEpiro <aguliyev45@gmail.com>
2024-06-10 22:31:56 +0200

    LogisticRegression

--- test.py
+++ test.py
@@ -1,37 +1,12 @@
-"""
-k_neighbors = np.arange(10, 25)
-
-pipeline = Pipeline(steps = [
-    ('imputer', SimpleImputer(strategy = 'median')),
-    ('scaler', StandardScaler()),
-    ('knn', KNeighborsClassifier())
-    ]
-)
-
-start = time.time()
-
-grid_search = GridSearchCV(
-    pipeline,
-    param_grid = {'knn__n_neighbors': k_neighbors},
-    cv = 5,
-    scoring = 'accuracy',
-    return_train_score = True,
-    n_jobs=8)
-
-
-
-grid_search.fit(pd.DataFrame(X_train), Y_train)
-
-end = time.time()
-
-print(grid_search.best_params_['knn__n_neighbors'])
-print(grid_search.best_score_)
-print(end - start)
-print(grid_search.score(pd.DataFrame(X_test), Y_test))
-"""
-
+from pandas_profiling import ProfileReport
 import pandas as pd
 
-data = pd.read_csv('weatherAUS_missing_values_inputed.csv')
+data = pd.read_csv('weatherAUS.csv')
 
-print(data.info())
+data = data.sample(frac = 0.1, random_state = 42)
+
+profile = ProfileReport(data, title = 'Weather in Australia')
+
+profile.to_notebook_iframe()
+
+profile.to_file("report_2.html")
